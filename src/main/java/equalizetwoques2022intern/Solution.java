d14aab2488d240519e7a9ea732a29bf56b5e0696
muttjimmypark <muttpark57@gmail.com>
2022-09-11 00:48:32 +0900

    [카카오 2022 - 두 큐 합 같게 만들기] 최종 풀이 성공

--- src/main/java/equalizetwoques2022intern/Solution.java
+++ src/main/java/equalizetwoques2022intern/Solution.java
@@ -2,14 +2,15 @@ package equalizetwoques2022intern;
 
 // 2022 블라인드인턴 - 두 큐 합 같게 만들기
 // 220907 20:10 - 21:58
-// 220908 15:55 - 17:55 1번풀이 시간통과 실패
+// 220908 15:55 - 17:55 1번풀이(직접) 시간통과 실패
 // 220910 21:35 - 22:32
-// 23:00 - 23:34 2번풀이 시간통과 실패 (11~28, 30)
-// 23:34 - 24:07 최종풀이 시간초과 (24만)
-// 24:20 -
+// 23:00 - 23:34 2번풀이(직접) 시간통과 실패 (11~28, 30)
+// 23:34 - 24:07 최종풀이(유튜브) 시간초과 (24만)
+// 24:20 - 24:46 풀이 완료
+// (378분)
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
@@ -17,8 +18,8 @@ class Solution {
         int queLength = queue1.length;
         long q1sum = 0L;
         long q2sum = 0L;
-        List<Integer> q1 = new ArrayList<>();
-        List<Integer> q2 = new ArrayList<>();
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
         for (int i = 0; i < queLength; i++) {
             int tmp = queue1[i];
             q1sum += tmp;
@@ -31,14 +32,14 @@ class Solution {
 
         for (int i = 0; i < 600000; i++) {
             if (q1sum > q2sum) {
-                int tmp = q1.remove(0);
+                int tmp = q1.poll();
                 q2.add(tmp);
                 q1sum -= tmp;
                 q2sum += tmp;
                 continue;
             }
             if (q1sum < q2sum) {
-                int tmp = q2.remove(0);
+                int tmp = q2.poll();
                 q1.add(tmp);
                 q1sum += tmp;
                 q2sum -= tmp;
