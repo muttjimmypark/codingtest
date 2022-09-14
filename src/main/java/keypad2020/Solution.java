b6f18ab84e0ed526ad1aba3d0e9080ddb71381a0
muttjimmypark <muttpark57@gmail.com>
2022-09-15 03:25:07 +0900

    [카카오 2020 - 키패드 누르기] 풀이 완

--- src/main/java/keypad2020/Solution.java
+++ src/main/java/keypad2020/Solution.java
@@ -1,18 +1,17 @@
 package keypad2020;
 
+// 2020 인턴십 - 키패드 누르기
 //04:50 - 06:10
-//19:15 -
+//19:15 - 20:45
+//02:00 - 03:23 (243m)
 
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution {
-    int lPosition = 7;
-    int rPosition = 9;
+public class Solution {
+    int lPosition = 30;
+    int rPosition = 32;
     char gHand;
-    int[] memo = new int[1 << 9];
 
     public String solution(int[] numbers, String hand) {
+
         gHand = hand.toUpperCase().charAt(0);
         StringBuilder answer = new StringBuilder();
 
@@ -24,46 +23,47 @@ class Solution {
 
     private char findNextFinger(int number) {
         //번호가 앞순서와 연속된 경우
-        if (lPosition == number) {
-            return 'L';
-        }
-        if (rPosition == number) {
-            return 'R';
-        }
+//        if (lPosition == number) {
+//            return 'L';
+//        }
+//        if (rPosition == number) {
+//            return 'R';
+//        }
 
         int divideThree = number % 3;
+        int numCode = positionToCode(number);
 
         //번호가 가운데줄인 경우
-        if (divideThree == 2 | number == 0) {
-            int lDistance = findDistance(lPosition, number);
-            int rDistance = findDistance(rPosition, number);
+        if (divideThree == 2 || number == 0) {
+            int lDistance = findDistance(lPosition, numCode);
+            int rDistance = findDistance(rPosition, numCode);
 
             if (lDistance == rDistance) {
                 if (gHand == 'L') {
-                    lPosition = number;
+                    lPosition = numCode;
                     return 'L';
                 }
-                rPosition = number;
+                rPosition = numCode;
                 return 'R';
             }
 
-            if (lDistance > rDistance) {
-                lPosition = number;
+            if (lDistance < rDistance) {
+                lPosition = numCode;
                 return 'L';
             }
 
-            rPosition = number;
+            rPosition = numCode;
             return 'R';
         }
 
         //번호가 한쪽 측면인 경우
         if (divideThree == 1) {
-            lPosition = number;
+            lPosition = numCode;
             return 'L';
         }
 
         if (divideThree == 0) {
-            rPosition = number;
+            rPosition = numCode;
             return 'R';
         }
 
@@ -71,47 +71,14 @@ class Solution {
     }
 
     private int findDistance(int p1, int p2) {
-        //들어오면 안되는 숫자들 처리
-        if (p1 < 0 | p1 > 9
-                | p2 < 0 | p2 > 9) {
-            return 99;
-        }
-        if (p1 == p2) {
-            return 0;
-        }
-        int orNum = (1 << p1) | (1 << p2);
+        return Math.abs((p1 / 10) - (p2 / 10))
+                + Math.abs((p1 % 10) - (p2 % 10));
+    }
 
-        //이미 연산한 값 메모불러오기
-        if (memo[orNum] != 0) {
-            return memo[orNum];
+    private int positionToCode(int position) {
+        if (position == 0) {
+            return 31;
         }
-
-        //0에 접근하려는 경우 처리
-        if (p1 == 0) {
-            memo[orNum] = findDistance(8, p2) + 1;
-            return memo[orNum];
-        }
-        if (p2 == 0) {
-            memo[orNum] = findDistance(p1, 8) + 1;
-            return memo[orNum];
-        }
-
-        //거리가 1인 경우 판별
-        List<Integer> closeNum = new ArrayList<>();
-        closeNum.add(p1 - 3);
-        closeNum.add(p1 - 1);
-        closeNum.add(p1 + 1);
-        closeNum.add(p1 + 3);
-        if (closeNum.contains(p2)) {
-            memo[orNum] = 1;
-            return 1;
-        }
-
-        //거리가 1 이상인 경우 재귀
-        memo[orNum] = closeNum.stream()
-                .mapToInt(p -> findDistance(p, p2))
-                .min().getAsInt()
-                + 1;
-        return memo[orNum];
+        return ((position - 1) / 3 * 10) + ((position - 1) % 3);
     }
 }
