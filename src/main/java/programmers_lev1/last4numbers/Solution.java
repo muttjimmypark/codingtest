56be3b6891c7bbcc1134950a41b38ddcaf1e23c8
muttjimmypark <muttpark57@gmail.com>
2022-09-29 16:53:41 +0900

    프로그래머스 스킬체크 lv1 조건충족

---
+++ src/main/java/programmers_lev1/last4numbers/Solution.java
@@ -0,0 +1,18 @@
+package programmers_lev1.last4numbers;
+
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder answer = new StringBuilder();
+
+        int len = phone_number.length();
+        for (int i = 0; i < len; i++) {
+            if (i < len - 4) {
+                answer.append('*');
+            } else {
+                answer.append(phone_number.charAt(i));
+            }
+        }
+
+        return answer.toString();
+    }
+}
---
+++ src/main/java/programmers_lev1/lotto/Solution.java
@@ -0,0 +1,37 @@
+package programmers_lev1.lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {0, 0};
+        int zeroCount = 0;
+        int matchCount = 0;
+        List<Integer> checkWin = Arrays.stream(win_nums).boxed().collect(Collectors.toList());
+
+        for (int input : lottos) {
+            if (input == 0) {
+                zeroCount++;
+                continue;
+            }
+
+            if (checkWin.contains(input)) {
+                matchCount++;
+            }
+        }
+        answer[0] = makeRank(matchCount + zeroCount);
+        answer[1] = makeRank(matchCount);
+
+        return answer;
+    }
+
+    private int makeRank(int i) {
+        if (i == 0) {
+            return 6;
+        }
+
+        return 7 - i;
+    }
+}
