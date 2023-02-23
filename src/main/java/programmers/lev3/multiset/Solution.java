7a3daf2e3c236d1a765b2e12425085c463f00056
muttjimmypark <muttpark57@gmail.com>
2023-02-23 23:35:34 +0900

    프로그래머스 lv3 1문제 풀이 (최고의 집합)

---
+++ src/main/java/programmers/lev3/multiset/Solution.java
@@ -0,0 +1,35 @@
+package programmers.lev3.multiset;
+
+import java.util.Arrays;
+
+class Solution {
+
+    int[] result;
+    int idx;
+
+    public int[] solution(int n, int s) {
+        if (s < n) {
+            return new int[]{-1};
+        }
+
+        result = new int[n];
+        Arrays.fill(result, 0);
+        idx = 0;
+
+        solu(n, s);
+        Arrays.sort(result);
+
+        return result;
+    }
+
+    private void solu(int n, int s) {
+        if (n == 1) {
+            result[idx++] = s;
+            return;
+        }
+
+        int av = s / n;
+        result[idx++] = av;
+        solu(n - 1, s - av);
+    }
+}
