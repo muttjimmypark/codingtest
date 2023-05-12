03c404f86ed7d21750b61382ada5cc9add871f63
muttjimmypark <muttpark57@gmail.com>
2023-05-12 21:09:53 +0900

    프로그래머스 2단계 한문제 더

--- src/main/java/programmers/lev2/arraymultipleminimum/Solution.java
+++ src/main/java/programmers/lev2/arraymultipleminimum/Solution.java
@@ -3,21 +3,16 @@ package programmers.lev2.arraymultipleminimum;
 import java.util.Arrays;
 
 class Solution {
-    int n;
-    int[][] multipleMap;
-
     public int solution(int[] A, int[] B) {
-        n = A.length;
-        multipleMap = new int[n][n];
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int n = A.length;
+        int answer = 0;
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                multipleMap[i][j] = A[i] * B[j];
-            }
+            answer += A[i] * B[n - i - 1];
         }
-
-        int answer = Integer.MAX_VALUE;
-
-        //
 
         return answer;
     }
